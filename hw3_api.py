ccd34415ecf95ff201b4ffca13fbaa69b7c39792
Shru Kumar <shrutika.skumar@gmail.com>
2024-03-13 02:32:50 -0400

    finalizing api and implementation

--- hw3_api.py
+++ hw3_api.py
@@ -1,12 +1,97 @@
+"""
+author: Shru Kumar
+filename: hw3_api.py
+description: establishes RedisAPI for node-edge graph functions
+"""
 import redis
 
 class RedisAPI:
     def __init__(self, host = 'localhost', port = 6379, decode_responses = True):
         self.r = redis.Redis(host = host, port = port, decode_responses = decode_responses)
         self.r.flushall()
+        self.r.set('edge_id', 0)
 
     def close(self):
         self.r.quit()
 
     def add_node(self, name, type):
-        self.r.lpush()
+        """Add a node to the database of a given name and type
+        Parameters:
+            name (str): name of node
+            type (str): what the node is
+        """
+        self.r.set(name, type)
+
+    def add_edge(self, name1, name2, type):
+        """Add an edge between nodes named name1 and name2. Type is the type of the edge or relationship
+        Parameters:
+            name1 (str): name of first node
+            name2 (str): name of second node
+            type (str): relationship
+        """
+        self.r.incr('edge_id')
+        edge_id = self.r.get('edge_id')
+        self.r.hset(edge_id, mapping = {'in_node': name1, 'out_node': name2, 'type': type})
+        self.r.rpush('edge_ids', edge_id)
+        
+    def get_adjacent(self, name, node_type=None, edge_type=None):
+        """Get the names of all adjacent nodes. User may optionally specify that the adjacent nodes are
+        of a given type and/or only consider edges of a given type. 
+        Parameters:
+            name (str): name of selected node
+            node_type (str): which type of nodes to filter for
+            edge_type (str): which type of edges to filter for
+        Returns:
+            adjacent_nodes (ls): list of adjacent nodes matching filters, if any
+        """
+        adjacent_nodes = []
+
+        # iterating through all edges
+        for edge_id in self.r.lrange('edge_ids', 0, -1):
+            if self.r.hget(edge_id, 'in_node') == name:
+                # storing nodes that are related to target node
+                adjacent_node = self.r.hget(edge_id, 'out_node')
+
+                # checking if node matches node and edge type filters, if any
+                if ((node_type is None) or (node_type == self.r.get(adjacent_node))) and \
+                    ((edge_type is None) or (edge_type == self.r.hget(edge_id, 'type'))):
+                        adjacent_nodes.append(adjacent_node)
+
+        return adjacent_nodes
+    
+    def get_recommendations(self, name):
+        """Get all books purchased by people that a given person knows but exclude 
+        books already purchased by that person.
+        Parameters:
+            name (str): name of person to recommend books to
+        Returns:
+            recommended_books (set): set of unique recommended books
+        """
+
+        # getting people target name knows and books they have bought
+        friends = self.get_adjacent(name, node_type = 'Person', edge_type = 'knows')
+        books = self.get_adjacent(name, node_type = 'Book', edge_type = 'bought')
+
+        # collecting unique books that friends have bought
+        friend_books = set()
+        for friend in friends:
+            for book in self.get_adjacent(friend, node_type = 'Book', edge_type = 'bought'):
+                friend_books.add(book)
+
+        # excluding books that target name has already bought
+        recommended_books = (friend_books - set(books))
+
+        return recommended_books
+            
+
+
+
+
+
+
+
+
+
+    
+
+    
--- hw3_recommend.py
+++ hw3_recommend.py
@@ -1,7 +1,44 @@
+"""
+author: Shru Kumar
+filename: hw3_recommend.py
+description: functions that the RedisAPI can perform
+"""
+
 from hw3_api import RedisAPI
-import pandas as pd
-import time
 
 def main():
     # establishing API connection
     api = RedisAPI()
+    api.r.flushall()
+
+    # adding nodes
+    api.add_node('Emily', 'Person')
+    api.add_node('Spencer', 'Person')
+    api.add_node('Brendan', 'Person')
+    api.add_node('Trevor', 'Person')
+    api.add_node('Paxton', 'Person')
+    api.add_node('Cosmos', 'Book')
+    api.add_node('Database Design', 'Book')
+    api.add_node('The Life of Cronkite', 'Book')
+    api.add_node('DNA and you', 'Book')
+
+    # adding edges
+    api.add_edge('Emily', 'Database Design', 'bought')
+    api.add_edge('Spencer', 'Cosmos', 'bought')
+    api.add_edge('Spencer', 'Database Design', 'bought')
+    api.add_edge('Brendan', 'Database Design', 'bought')
+    api.add_edge('Brendan', 'DNA and you', 'bought')
+    api.add_edge('Trevor', 'Cosmos', 'bought')
+    api.add_edge('Trevor', 'Database Design', 'bought')
+    api.add_edge('Paxton', 'Database Design', 'bought')
+    api.add_edge('Paxton', 'DNA and you', 'bought')
+    api.add_edge('Emily', 'Spencer', 'knows')
+    api.add_edge('Spencer', 'Emily', 'knows')
+    api.add_edge('Spencer', 'Brendan', 'knows')
+
+    # getting recommendations for spencer
+    spencer_recommended = api.get_recommendations('Spencer')
+    print(spencer_recommended)
+
+if __name__ == '__main__':
+    main()
